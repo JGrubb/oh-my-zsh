52ca765959339657ae36f8a6b92f94bc3d73367b
John Grubb <therealjohnnygrubb@gmail.com>
2013-04-04 11:05:46 -0400

    updating af-magic

--- plugins/utils/utils.plugin.zsh
+++ plugins/utils/utils.plugin.zsh
@@ -1,3 +1,1 @@
-CURRENT_DIR = ${PWD##*/}
-
-alias sync="rsync -av --exclude=files/js --exclude=files/css --exclude=files/feeds prod:prod/sites/$CURRENT_DIR/files ."
+alias sync="rsync -av --exclude=files/js --exclude=files/css --exclude=files/feeds prod:prod/sites/${PWD##*/}/files ."
